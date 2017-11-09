bfc44701f57ac12f16e7311d55f7593ff9d77a8a
Nicole S <nyscherm@gmail.com>
2017-11-08 23:20:54 -0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+	var imgUrl = "";
+	var galleryID = " ";
+	var $this = null;
+	var count = 1;
+
+	$('.image').click(function() {
+		$('.modal').css('visibility','visible');
+		console.log("click");
+		imgUrl = $(this).css('background-image');
+		$('.modal .image').css('background-image', imgUrl);
+		$('.modal .image').css('opacity', '1');
+	});
+
+	$('.modal').click(function() {
+		$('.modal').css('visibility','hidden');
+		$('.modal .image').css('opacity', '0');
+	});
+
+	var headerContent = '' +
+	'<div class="left-align">' +
+		'<h2><a href="index.html">Misha Naiman</a></h2>' +
+		'<h3>Majorelle Arts</h3>' +
+	'</div>' +
+	'<div class="right-align">' +
+		'<a href="30w.html">30th and West Live/Work</a>' +
+		'<a href="art.html">Art</a>' +
+		'<a href="science.html">Science</a>' +
+		'<a href="personal.html">Personal Projects</a>' +
+		'<a href="about.html">About</a>' +
+	'</div>';
+
+	var footerContent = '' +
+	'<div class="left-align">&copy; 2017 Majorelle Arts, Misha Naiman</div>' +
+	'<div class="right-align">' +
+		'<a href="credits.html">Credits</a> <a href="mailto:msnaiman12@gmail.com">Contact</a>' +
+	'</div>';
+
+	$('header').append(headerContent);
+	$('footer').append(footerContent);	
+
+	$('.gallery').each(function(){
+		$this = $(this);
+		galleryID = $this.attr('id');
+		$this.find($('.image')).each(function(){
+			console.log($(this));
+			imgUrl = 'url("images/'+galleryID+'-'+count+'.jpg")';
+			$(this).css('background-image', imgUrl);
+			count++;
+		});
+		count=1;
+	});
+});	
